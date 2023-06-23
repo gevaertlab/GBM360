3d779281233660ff98968e961c303276891fa61c
Yuanning Zheng <yuanningzheng@DNa828061.SUNet>
2023-06-23 16:34:48 -0700

    update tutorial

--- src/about.py
+++ src/about.py
@@ -23,7 +23,8 @@ def app():
         paragraph = "<span style='font-size: 18px;'>- Dr. Yuanning Zheng is a postdoctoral scholar at Stanford University. He obtained his PhD degree in Medical Sciences from Texas A&M University and a Master in Computer Science from Georgia Institute of Technology.</span>\n" \
                     "<span style='font-size: 18px;'>Dr. Zheng's research focuses on developing innovative machine learning and bioinformatics methods to unravel the heterogeneity and improve personalized diagnosis of cancers and other complex diseases. Email: eric2021@stanford.edu</span>\n\n" \
                     "<span style='font-size: 18px;'>- Dr. Olivier Gevaert is an associate professor at Stanford University focusing on developing machine-learning methods for biomedical decision support from multi-scale data. Email: ogevaert@stanford.edu</span>\n\n" \
-                    "<span style='font-size: 18px;'>- Visit us at: <a href='https://med.stanford.edu/gevaertlab.html'>Dr. Gevaert lab</a></span>"
+                    "<span style='font-size: 18px;'>- Visit us at: <a href='https://med.stanford.edu/gevaertlab.html'>Dr. Gevaert lab</a></span>\n\n" \
+                    "<span style='font-size: 18px;'>- For bug reporting, please visit: <https://github.com/gevaertlab/GBM360></span>"
 
         st.markdown(paragraph, unsafe_allow_html=True)
     
